bbffcc9cb8c8fca0cdd246ae76c9fdd3aac83b9f
Reason <1024053131@qq.com>
2019-04-03 20:49:42 +0800

    02-4/3当天任务提交(以完成主页部分)

---
+++ js/index.js
@@ -0,0 +1,31 @@
+$(function () {
+  // 轮播图动态渲染
+  $.ajax({
+    type: 'get',
+    url: 'home/swiperdata',
+    dataType: 'json',
+    success: function (result) {
+      // 生成dom结构代码
+      let html = template('pyg_sliderTemp', result);
+      $('.pyg_slide_image').html(html);
+      // 生成小圆点结构代码
+      let dat = template('pyg_datTemp', result);
+      $('.pyg_dat').html(dat);
+      //MUI中动态生成的结构需要手动初始化
+      var gallery = mui('.mui-slider');
+      gallery.slider({
+        interval: 1000 //自动轮播周期，若为0则不自动播放，默认为0；
+      });
+    }
+  })
+  // 产品部分动态渲染
+  $.ajax({
+    type: 'get',
+    url: 'home/goodslist',
+    dataType: 'json',
+    success: function (result) {
+      let html = template('pyg_dataTemp', result);
+      $('.pyg_data').html(html)
+    }
+  })
+})
